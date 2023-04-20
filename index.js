156cf73b0a8e9fbf61b2fab4ac0527fd708b7361
Svyat <swcnova@gmail.com>
2023-04-20 22:49:31 +0700

    major fix: watch readme

--- index.js
+++ index.js
@@ -2,52 +2,89 @@ const searchInput = document.querySelector('.search__input');
 const searchButton = document.querySelector('.search__button');
 const forecastSection = document.querySelector('.forecast');
 
-const temperature = document.querySelector('.temperature');
-const description = document.querySelector('.description');
+
+const place = document.querySelector('.forecast__place');
+const countryPlace = document.querySelector('.forecast__country');
+const temperature = document.querySelector('.forecast__temperature');
+const description = document.querySelector('.forecast__description');
+const pressure = document.querySelector('#pressure')
 const humidity = document.querySelector('#humidity');
 const windSpeed = document.querySelector('#wind-speed');
-const weatherIco = document.querySelector('.precipitation-img');
+const weatherIco = document.querySelector('.forecast__precipitation-img');
+const feelsLike = document.querySelector('#feels-like')
 const error400 = document.querySelector('.error400');
 
-searchButton.addEventListener('click', () => {
-    const APIKey = '665b2b0576d8479dba0134633231504';
-    const city = searchInput.value;
+const setLocation = ({name, country}) => {
+    place.textContent = `${name}`
+    countryPlace.textContent = `${country}`
+};
 
-    if (city === '') { return }
+const setDigits = (current) => {
+    console.log(current)
+    
+    temperature.textContent = `${current.temp_c}°C`;
+    description.textContent = current.condition.text;
+    pressure.textContent = `${current.pressure_mb} mB`;
+    humidity.textContent = `${current.humidity}%`;
+    windSpeed.textContent = `${current.wind_kph}Km/h`;
+    feelsLike.textContent = `${current.feelslike_c}°C`;
 
-    fetch(`https://api.weatherapi.com/v1/current.json?key=${APIKey}&q=${city}&aqi=no`)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        temperature.textContent = `${data.current.temp_c}°C`
-        description.textContent = data.current.condition.text
-        humidity.textContent = `${data.current.humidity}%`
-        windSpeed.textContent = `${data.current.wind_kph}Km/h`
-        error400.classList.remove('error400_active')
-        forecastSection.classList.add('forecast_active');
-        switch (data.current.condition.text) {
-            case 'Clear':
-                weatherIco.src = './images/clear.png';
-                break;
-            case 'rain':
-                weatherIco.src = './images/rain.png';
-                break;
-            case 'mist':
-                weatherIco.src = './images/mist.png';
-                break;
-            case 'cloud':
-                weatherIco.src = './images/cloud.png';
-                break;
-            case 'snow':
-                weatherIco.src = './images/snow.png';
-                break;
-            default:
-                weatherIco.src = data.current.condition.icon;
-        }
-    })        
+    hideError()
+    
+};
 
-    .catch(() => {
-            forecastSection.classList.remove('forecast_active')
-            error400.classList.add('error400_active')        
-    })
-})
+const setIco = ({text, icon}) => {
+    switch (text) {
+
+    case 'Clear':
+        weatherIco.src = './images/clear.png';
+        break;
+    case 'rain':
+        weatherIco.src = './images/rain.png';
+        break;
+    case 'mist':
+        weatherIco.src = './images/mist.png';
+        break;
+    case 'cloud':
+        weatherIco.src = './images/cloud.png';
+        break;
+    case 'snow':
+        weatherIco.src = './images/snow.png';
+        break;
+    default:
+        weatherIco.src = icon;
+    }
+};
+
+const hideError = () => {
+    error400.classList.remove('error400_active');
+    forecastSection.classList.add('forecast_active');
+};
+
+const showError = () => {
+    forecastSection.classList.remove('forecast_active');
+    error400.classList.add('error400_active');
+};
+            
+const getForecast = () => {
+        const APIKey = '665b2b0576d8479dba0134633231504';
+        const city = searchInput.value;
+
+        if (city === '') { return }
+                
+        fetch(`https://api.weatherapi.com/v1/current.json?key=${APIKey}&q=${city}&aqi=no`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setLocation(data.location)
+                setDigits(data.current)
+                setIco(data.current.condition)    
+            })        
+                
+        .catch((err) => {
+            showError()
+            console.log(err.message)    
+        })
+};
+
+searchButton.addEventListener('click', () => getForecast());
